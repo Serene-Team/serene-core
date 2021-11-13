89bc1a728738196a540afb9f4ce8e64aa3ac59e3
Interfiber <webmaster@interfiber.dev>
2021-11-13 15:54:46 -0500

    :bug: Fix bugs with player adding system

--- src/ReplicatedStorage/modules/player.lua
+++ src/ReplicatedStorage/modules/player.lua
@@ -6,55 +6,34 @@ local statPoints = require(game.ReplicatedStorage:WaitForChild("modules").player
 local teleportServer = require(game.ReplicatedStorage:WaitForChild("modules").teleportServer)
 local currency = require(game.ReplicatedStorage:WaitForChild("modules").currency)
 local extra = require(game.ReplicatedStorage:WaitForChild("modules").extra)
-module.playerAdded = function(player)
-	if player:GetAttribute("hasBeenAdded") == nil then
-		player:SetAttribute("hasBeenAdded", true)
-		print("loading in player info for "..player.Name)
-		local backpackFolder = Instance.new("Folder")
-		backpackFolder.Name = player.Name
-		backpackFolder.Parent = game.ServerStorage
-		player.CharacterAdded:Connect(function()
-			backpack.loadBackpack(player)
-			backpack.autoSave(player)
-			-- load player info
-			levels.loadLevelData(player)
-		end)
-		currency.currencyUpdated(player, function(currency, eventType)
-			game.ReplicatedStorage:WaitForChild("events"):WaitForChild("playLocalSoundEffect"):FireClient(player, "getCoins")
-			extra.sendCurrencyAlert(player, currency, eventType)
-		end)
-		statPoints.applyStatPoints(player)
-		if game.PlaceId ~= 7193001633 then
-			teleportServer.saveCurrentPlace(player)
-		end
-	end
-end
-module.playerForceAdd = function (player)
-	if player:GetAttribute("hasBeenAdded") == nil then
-		player:SetAttribute("hasBeenAdded", true)
-		print("loading in player info for "..player.Name)
-		local backpackFolder = Instance.new("Folder")
-		backpackFolder.Name = player.Name
-		backpackFolder.Parent = game.ServerStorage
+function addPlayer(player)
+	print("loading in player info for "..player.Name)
+	local backpackFolder = Instance.new("Folder")
+	backpackFolder.Name = player.Name
+	backpackFolder.Parent = game.ServerStorage
+	player.CharacterAdded:Connect(function()
 		backpack.loadBackpack(player)
 		backpack.autoSave(player)
 		-- load player info
 		levels.loadLevelData(player)
 		statPoints.applyStatPoints(player)
-		player.CharacterAdded:Connect(function ()
-			backpack.loadBackpack(player)
-			-- load player info
-			levels.loadLevelData(player)
-			statPoints.applyStatPoints(player)
-		end)
-		currency.currencyUpdated(player, function(currency, eventType)
-			game.ReplicatedStorage:WaitForChild("events"):WaitForChild("playLocalSoundEffect"):FireClient(player, "getCoins")
-			extra.sendCurrencyAlert(player, currency, eventType)
-		end)
-		if game.PlaceId ~= 7193001633 then
-			teleportServer.saveCurrentPlace(player)
-		end
+	end)
+	currency.currencyUpdated(player, function(currency, eventType)
+		game.ReplicatedStorage:WaitForChild("events"):WaitForChild("playLocalSoundEffect"):FireClient(player, "getCoins")
+		extra.sendCurrencyAlert(player, currency, eventType)
+	end)
+	if game.PlaceId ~= 7193001633 then
+		teleportServer.saveCurrentPlace(player)
 	end
+end
+module.playerAdded = function(player)
+	if player:GetAttribute("hasBeenAdded") == nil then
+		player:SetAttribute("hasBeenAdded", true)
+		addPlayer(player)
+	end
+end
+module.playerForceAdd = function (player)
+	addPlayer(player)
 end
 
 return module
